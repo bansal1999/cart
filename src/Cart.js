27b27325ba51613bfc6967f9b4fa91d5bf531347
ShikharBansal <shikharbansal007@gmail.com>
2021-01-02 10:48:06 +0530

    added cart components 2021

--- src/Cart.js
+++ src/Cart.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-const Cart = (props) => {
-  const { products } = props;
-  return (
-    <div className="cart">
-      {products.map((product) => {
-        return (
-          <CartItem
-            product={product}
-            key={product.id}
-            onIncreaseQuantity={props.onIncreaseQuantity}
-            onDecreaseQuantity={props.onDecreaseQuantity}
-            onDeleteProduct={props.onDeleteProduct}
-          />
-        )
-      })}
-    </div>
-  );
+class Cart extends React.Component {
+  render () {
+    return (
+      <div className="cart">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+    );
+  }
 }
 
 export default Cart;
