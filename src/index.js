30067764fd359c00cc95b79f71d762d41d85fca3
ShikharBansal <shikharbansal007@gmail.com>
2021-02-10 13:14:35 +0530

    Completed the firebase setup in cart

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import firebase from "firebase/app"
-import 'firebase/firestore';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from 'firebase/app'
+import "firebase/firestore";
 
-
-  // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDeyKQgIDOcli9e1lWzIJAIaDvqhID8T5Q",
-    authDomain: "cart-3a846.firebaseapp.com",
-    projectId: "cart-3a846",
-    storageBucket: "cart-3a846.appspot.com",
-    messagingSenderId: "564379963206",
-    appId: "1:564379963206:web:c8aa02bda829fc0e85df47"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDeyKQgIDOcli9e1lWzIJAIaDvqhID8T5Q",
+  authDomain: "cart-3a846.firebaseapp.com",
+  projectId: "cart-3a846",
+  storageBucket: "cart-3a846.appspot.com",
+  messagingSenderId: "564379963206",
+  appId: "1:564379963206:web:c8aa02bda829fc0e85df47"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
